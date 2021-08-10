c55d235761c383408984706738fa0d86fa288950
m83115 <m83115@yahoo.com.tw>
2021-08-10 21:15:25 +0800

    noteday03

--- app/controllers/notees_controller.rb
+++ app/controllers/notees_controller.rb
@@ -1,21 +1,29 @@
 class NoteesController < ApplicationController
     def index
-
+        # @notees = Notee.all.sort.reverse X
+        @notees = Notee.order(id: :desc)
     end
 
     def new
         @notee = Notee.new
     end
 
-    def create
-        title = params[:title]
-        content = params[:content]
-        notee = Notee.new(title: title, content: content)
+    def show
+        @notee = Notee.find(params[:id])
+        # ORM
+        # select * from notes where id =2  
+    end
 
-        if notee.save
+    def create
+        #Strong Parameter 強參數
+        clean_notee = params.require(:notee).permit(:title, :content)
+        @notee = Notee.new(clean_notee)
+
+        if @notee.save
             redirect_to "/notees"
         else
-        
+            render :new
+            # redirect_to "/notees/new"
         end
 
     end
--- app/models/notee.rb
+++ app/models/notee.rb
@@ -1,2 +1,3 @@
 class Notee < ApplicationRecord
+  validates :title, presence: true
 end
