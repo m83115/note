8ab6bdbc8a0a5abe74343437f27edeb74cc54b87
m83115 <m83115@yahoo.com.tw>
2021-08-13 18:15:02 +0800

    up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+
+<% if note.errors.any? %>
+    <ul>
+        <% note.errors.full_messages.each do |message| %>
+            <li><%= message %></li>
+        <% end %>
+    </ul>
+<% end %>
+
+rescue_from ActiveRecord::RecordNotFound, with::recode_not_found
+private
+
+def record_not_found
+    render file: "public/404.html", status: :not_found
 end
+
--- app/controllers/notees_controller.rb
+++ app/controllers/notees_controller.rb
@@ -1,4 +1,6 @@
 class NoteesController < ApplicationController
+    before_action :find_notee, only: [:show, :edit, :update, :destroy]
+
     def index
         # @notees = Notee.all.sort.reverse X
         @notees = Notee.order(id: :desc)
@@ -8,16 +10,22 @@ class NoteesController < ApplicationController
         @notee = Notee.new
     end
 
-    def show
+    def edit
         @notee = Notee.find(params[:id])
+        # ORM
+        # select * from notes where id =2  
+    end
+
+    def show
+        # @notee = Notee.find(params[:id])
         # ORM
         # select * from notes where id =2  
     end
 
     def create
         #Strong Parameter 強參數
-        clean_notee = params.require(:notee).permit(:title, :content)
-        @notee = Notee.new(clean_notee)
+        
+        @notee = Notee.new(note_params)
 
         if @notee.save
             redirect_to "/notees"
@@ -25,7 +33,34 @@ class NoteesController < ApplicationController
             render :new
             # redirect_to "/notees/new"
         end
-
     end
 
+    def update
+    
+        # @notee = Notee.find(params[:id])
+        
+        if @notee.update(note_params)
+            redirect_to "/notees"
+        else
+            render :edit
+            # redirect_to "/notees/new"
+        end
+    end    
+
+    def destroy
+        # @notee = Notee.find(params[:id])
+        @notee.destroy
+        redirect_to "/notees"
+    end
+
+    private
+    def note_params
+        params.require(:notee).permit(:title, :content)
+    end
+
+    def find_notee
+
+        render file: "public/404.html", status: :not_found
+
+    end
 end
